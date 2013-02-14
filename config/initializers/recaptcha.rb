ed1b2594410d891a47dbbfa4a00db7cbd6264b71
Barnaba Turek <barnabaturek@gmail.com>
2013-02-14 06:39:23 +0100

    recaptcha keys

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6Lch_9wSAAAAAHfpfe_lWVUnhQsO7URKirWek-lu'
-  config.private_key = '6Lch_9wSAAAAAEAmt4JlIpQ3Uz__MQlUztap60hk'
+  config.public_key  = '6LfN_9wSAAAAAHiZEFRtrCGRUnkPq2Hd-XuYnhWv'
+  config.private_key = '6LfN_9wSAAAAACCAL9I2gXbVT_bCr1SRs36cUrAN'
 end
