aaf27d0aa94263486a2ff8bd3b2ced7b1ee4f2f6
Barnaba Turek <barnabaturek@gmail.com>
2013-02-14 02:59:46 +0100

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmsonrails::Application.config.secret_token = '2f9752e6a34855061f5dede9949f4ecf16f9c5cf4c7b62e1e648a1c42530e9ef76a413bb09ad852a1abbc6c02fef6538062a1643560bcd1a7fbbb3bb0a4bd73f'
