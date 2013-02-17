9a53fa613be2aefa77a50614516c6c5b9a9dc20d
Barnaba Turek <barnabaturek@gmail.com>
2013-02-18 00:04:52 +0100

    podswietlanie aktualnej podstrony

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def active_if(action_string)
+    (controller_name, action_name) = action_string.split('#')
+    if params[:controller] == controller_name and params[:action] == action_name
+      'class=active'
+    else
+      ''
+    end
+  end
 end
