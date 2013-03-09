c9daa8322e5cd197cb5d978d76f2bd3fecdf270c
Barnaba Turek <barnabaturek@gmail.com>
2013-03-09 03:04:36 +0100

    mailer

---
+++ app/mailers/attendee_mailer.rb
@@ -0,0 +1,17 @@
+#encoding: utf-8
+class AttendeeMailer < ActionMailer::Base
+  default from: "rejestracja@rms2013.pl"
+
+  def registration_email(attendee)
+    @name= attendee.name
+    @email = attendee.email
+    @registration_type = attendee.actual_registration_type
+    @registration_type_text = case @registration_type
+                              when "seated"
+                                "Miejsce siedzące"
+                              when "standing"
+                                "Miejsce stojące"
+                              end
+    mail(to: attendee.email, subject: "Potwierdzenie rejestracji na wykład Richarda Stallmana")
+  end
+end
