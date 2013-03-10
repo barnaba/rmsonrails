d345bee29b369b8ff3776659e1779016164e6d02
Barnaba Turek <barnabaturek@gmail.com>
2013-03-10 14:21:42 +0100

    draft maila przypominającego - #11

--- app/mailers/attendee_mailer.rb
+++ app/mailers/attendee_mailer.rb
@@ -14,4 +14,11 @@ class AttendeeMailer < ActionMailer::Base
                               end
     mail(to: attendee.email, subject: "Potwierdzenie rejestracji na wykład Richarda Stallmana", content_type: "text/plain")
   end
+
+  def reminder_email(attendee)
+    @name= attendee.name
+    @email = attendee.email
+    @registration_type = attendee.actual_registration_type
+    mail(to: attendee.email, subject: "Przypomnienie o wykładzie Richarda Stallmana, godziny rejestracji", content_type: "text/plain")
+  end
 end
