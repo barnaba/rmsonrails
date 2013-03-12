e54dd15795977966da0af6a4aa015d0ff970b7d3
Barnaba Turek <barnabaturek@gmail.com>
2013-03-12 16:20:10 +0100

    podswietlanie streama, gdy jest aktywny

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
+#encoding: utf-8
+require 'net/http'
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :stream_active?
+
+  def stream_active?
+    begin
+      url = URI.parse('http://brama.elka.pw.edu.pl/stream/606')
+      req = Net::HTTP::Get.new(url.path)
+      res = Net::HTTP.start(url.host, url.port) {|http|
+          http.request(req)
+      }
+      active = !(res.body =~ /Strumieniowanie jeszcze nie rozpocz/)
+      puts active.inspect
+      @stream_active = active
+    rescue
+      @stream_active = false
+    end
+  end
 end
